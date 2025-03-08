72885cb2107d803109c7bf1350de4b85e2f992e0
Tequinzyy <tonyminhquan450@gmail.com>
2025-03-09 03:39:47 +0700

    merge queries into 1

--- updateDatabase.sql
+++ updateDatabase.sql
@@ -34,35 +34,6 @@ alter table Products
 add start_rate INT NULL;
 GO
 
-create table Comments(
-	id INT PRIMARY KEY IDENTITY(1,1),
-    userID NVARCHAR(255) NOT NULL,
-	product_id INT,
-    name NVARCHAR(255),
-	start INT NULL,
-	created_at DATETIME DEFAULT GETDATE(),
-    updated_at DATETIME DEFAULT GETDATE()
-);
-
--- update 09/03/2025
-ALTER TABLE Comments
-ADD content NVARCHAR(250) NULL;
-
-ALTER TABLE Comments
-ALTER COLUMN userID INT NOT NULL;
-
-ALTER TABLE Comments
-ALTER COLUMN product_id INT NOT NULL;
-
-ALTER TABLE Comments
-ADD CONSTRAINT FK_Comments_Users FOREIGN KEY (userID) REFERENCES Users(id);
-
-ALTER TABLE Comments
-ADD CONSTRAINT FK_Comments_Products FOREIGN KEY (product_id) REFERENCES Products(id);
-
-EXEC sp_rename 'Comments.start', 'stars', 'COLUMN';
-
-
 -- update 04/03/2025
 alter table Promotion
 add code VARCHAR(10) NULL;
@@ -82,3 +53,17 @@ GO
 
 -- update 06/03/2025
 update Products set start_rate = 0
+
+-- update-2 09/03/2025
+CREATE TABLE Comments(
+    id INT PRIMARY KEY IDENTITY(1,1),
+    userID INT NOT NULL,
+    product_id INT NOT NULL,
+    name NVARCHAR(255),
+    stars DECIMAL(2,1) NOT NULL,
+    content NVARCHAR(250) NULL,
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME DEFAULT GETDATE(),
+    FOREIGN KEY (userID) REFERENCES Users(id),
+    FOREIGN KEY (product_id) REFERENCES Products(id)
+);
