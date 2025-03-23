f7f258a822825c08fe2304a70faddad90cbd3e35
Minh Quân <116754124+Tequinzyy@users.noreply.github.com>
2025-03-24 02:37:53 +0700

    Update config.js

--- user/src/config.js
+++ user/src/config.js
@@ -1,5 +1,5 @@
 // Dev URL
-// export const API_URL = 'https:/localhost/:7192';
+export const API_URL = 'https://localhost:7192';
 
 // Prod URL
-export const API_URL = 'https://mobileshopapi.runasp.net';
+// export const API_URL = 'https://mobileshopapi.runasp.net';
