a4c5d9b9f13fc4535a296d96aa6e9c90b869b9c3
Administrator <gitlab_admin_11f6f4@example.com>
2026-01-03 07:37:55 +0000

    Fix: Update API_URL to server IP

--- user/src/config.js
+++ user/src/config.js
@@ -1,4 +1,5 @@
 // Cho ai có file env
-export const API_URL = import.meta.env.VITE_API_BASE_URL;
+//export const API_URL = import.meta.env.VITE_API_BASE_URL;
+export const API_URL = "http://192.168.70.103:5000";
 
 // export const API_URL = "https://localhost:7192";
